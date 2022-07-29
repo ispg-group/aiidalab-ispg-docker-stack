b8550136924de5b51cb964a72ee50e7dc11555a5
Daniel Hollas <danekhollas@gmail.com>
2022-07-29 12:29:35 +0100

    HTTPS connection to Jupyter notebook (#6)
    * Add HTTPS instructions to README

---
+++ opt/start-notebook.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -e
+
+# ISPG: Added HTTPS by adding path to certificate and key in our Docker image
+
+# Debugging.
+set -x
+
+# Environment.  export SHELL=/bin/bash
+
+# Enter home folder and start jupyterhub-singleuser.
+cd /home/${SYSTEM_USER}
+
+if [[ ! -z "${JUPYTERHUB_API_TOKEN}" ]]; then
+
+  # Launched by JupyterHub, use single-user entrypoint.
+  /usr/bin/python3 /opt/aiidalab-singleuser                          \
+      --ip=0.0.0.0                                                   \
+      --port=8888                                                    \
+      --notebook-dir="/home/${SYSTEM_USER}"                          \
+      --ContentsManager.allow_hidden=True                            \
+      --VoilaConfiguration.template=aiidalab                         \
+      --VoilaConfiguration.enable_nbextensions=True                  \
+      --NotebookApp.iopub_data_rate_limit=1000000000                 \
+      --NotebookApp.default_url="/apps/apps/home/start.ipynb"
+else
+
+  # Otherwise launch notebook server directly.
+  /usr/local/bin/jupyter-notebook \
+      --ip=0.0.0.0 \
+      --port=8888 \
+      --no-browser \
+      --ContentsManager.allow_hidden=True \
+      --notebook-dir="/home/${SYSTEM_USER}" \
+      --NotebookApp.default_url="/apps/apps/home/start.ipynb" \
+      --certfile=/opt/certificates/localhost.crt \
+      --keyfile=/opt/certificates/localhost.key
+
+# I am not even sure that Voila currently works.
+#      --VoilaConfiguration.template=aiidalab                         \
+#      --VoilaConfiguration.enable_nbextensions=True                  \
+fi
