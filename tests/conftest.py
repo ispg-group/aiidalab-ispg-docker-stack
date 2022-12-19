0ced462cdb13eb4fa65c1e1635cc87229fabd152
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2022-12-19 18:05:40 +0000

    Do not verify SSL certificate

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,7 @@ from requests.exceptions import ConnectionError
 
 def is_responsive(url):
     try:
-        response = requests.get(url)
+        response = requests.get(url, verify=False)
         if response.status_code == 200:
             return True
         else:
